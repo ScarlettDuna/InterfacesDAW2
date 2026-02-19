437ff3c71c55389a61f75543ed3f9e67e5077049
Arantxa Reinoso <154524564+ScarlettDuna@users.noreply.github.com>
2026-02-19 14:18:55 +0100

    añadir countdown y carousel

--- Tarea5-6/app.js
+++ Tarea5-6/app.js
@@ -1,7 +1,8 @@
 // Modo sugerencia 
 let btnSugerencia = document.getElementById('suggestions');
+let btnFiltro = document.getElementById('filtro')
 let sugerenciasActivas = false;
-btnSugerencia.addEventListener('click', (e) => {
+btnSugerencia.addEventListener('click', () => {
     if (!sugerenciasActivas) {
         sugerenciasActivas = true;
         btnSugerencia.classList.add('activo');
@@ -15,37 +16,75 @@ btnSugerencia.addEventListener('click', (e) => {
 })
 
 
-// Búsqueda interactiva
-if (sugerenciasActivas) {
-    let carreras = [];
-    async function cargarCarreras() {
-    const res = await fetch('./races.json');
-    const data = await res.json();
-    carreras = data;
-    }
+// Sección de coundowns para proximas carreras
 
-    cargarCarreras();
-    let buscador = document.getElementById('inputBuscador')
-    let resultadoBusqueda = document.querySelector('.resultados ul')
+let countdownTokio = new Date("2026-03-01T09:10:00+09:00").getTime();
+let countdownBoston = new Date("2026-04-20T09:00:00-04:00").getTime();
+let countdownLondres = new Date("2026-04-26T09:30:00+01:00").getTime();
+let countdownMadrid = new Date("2026-04-26T08:45:00+02:00").getTime();
+let timerTokio = document.querySelector('.countdown1');
+let timerBoston = document.querySelector('.countdown2');
+let timerMadrid = document.querySelector('.countdown3');
+let timerLondres = document.querySelector('.countdown4');
 
-    buscador.addEventListener('input', () => {
-        let busqueda = buscador.value.trim();
-        if (busqueda === '') {
-            resultadoBusqueda.innerHTML = '';
+function timerFunction(countdown, parrafoSalida) {
+    let idInterval = setInterval(() => {
+        let now = new Date().getTime();
+
+        // Find the distance between now and the count down date
+        let distance = countdown - now;
+
+        // If the count down is finished, write some text
+        if (distance < 0) {
+            clearInterval(idInterval);
+            parrafoSalida.innerHTML = "Ya pasó";
             return;
         }
-        let opciones = []
-        for (let carrera of carreras) {
-            if (carrera['name'].includes(busqueda)) {
-                opciones.push(carrera['name'])
-            }
-        }
-        // limitamos el número de resultados mostrados a 5
-        opciones = opciones.slice(0, 5);
-        for (let opcion of opciones) {
-            let newElem = document.createElement('li')
-            newElem.innerText = opcion
-            resultadoBusqueda.appendChild(newElem)
-        }
-    })
+
+        // Time calculations for days, hours, minutes and seconds
+        let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        let minFormatted = String(minutes).padStart(2, '0');
+        let secFormatted = String(seconds).padStart(2, '0');
+
+        // Display the result 
+        parrafoSalida.innerText = `Quedan: ${days}d ${hours}:${minFormatted}:${secFormatted}`;
+        
+    }, 1000);
+}
+
+timerFunction(countdownTokio, timerTokio)
+timerFunction(countdownBoston, timerBoston)
+timerFunction(countdownMadrid, timerMadrid)
+timerFunction(countdownLondres, timerLondres)
+
+// Carousel Cerca de ti
+const track = document.querySelector('.carousel-track');
+const nextBtn = document.querySelector('.next');
+const prevBtn = document.querySelector('.prev');
+
+let index = 0;
+const cardWidth = track.children[0].offsetWidth;
+const visibleCards = 4;
+
+nextBtn.addEventListener('click', () => {
+    const totalCards = track.children.length;
+
+    if (index < totalCards - visibleCards) {
+        index++;
+        updateCarousel();
+    }
+});
+
+prevBtn.addEventListener('click', () => {
+    if (index > 0) {
+        index--;
+        updateCarousel();
+    }
+});
+
+function updateCarousel() {
+    track.style.transform = `translateX(-${index * cardWidth}px)`;
 }
